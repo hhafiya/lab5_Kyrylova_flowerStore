35fd37b1a5c5eb130f69a5f7c2763f7ff262317a
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-10-13 21:56:04 +0300

    Update FlowerPack.java

--- src/main/java/flower/store/FlowerPack.java
+++ src/main/java/flower/store/FlowerPack.java
@@ -8,11 +8,11 @@ public class FlowerPack {
     private Flower flower;
     private int quantity;
 
-    public FlowerPack(Flower flower, int quantity){
+    public FlowerPack(Flower flower, int quantity) {
         this.flower = new Flower(flower);
         this.quantity = quantity;
     }
-    public double getPrice(){
+    public double getPrice() {
         return flower.getPrice() * quantity;
     }
     
