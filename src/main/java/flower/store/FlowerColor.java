50f173082eede8149504eabbf69b8f0fe225eb4c
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-10-13 21:55:30 +0300

    Update FlowerColor.java

--- src/main/java/flower/store/FlowerColor.java
+++ src/main/java/flower/store/FlowerColor.java
@@ -1,14 +1,15 @@
 package flower.store;
 
 public enum FlowerColor {
-    RED("#FF0000") , GREEN("#008000"), BLUE("#0000FF"), YELLOW("#FFFF00"), WHITE("#FFFFFF");
+    RED("#FF0000"), GREEN("#008000"),
+    BLUE("#0000FF"), YELLOW("#FFFF00"), WHITE("#FFFFFF");
     private final String rgb;
 
     FlowerColor(String rgb) {
         this.rgb = rgb;
     }
 
-    public String toString(){
+    public String toString() {
         return rgb.toString();
     }
 }
