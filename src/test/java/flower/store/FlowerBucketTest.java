d914e35835fdef6323248cff49989c751c9f9fca
hhafiya <149326005+hhafiya@users.noreply.github.com>
2024-10-13 22:35:06 +0300

    Update FlowerBucketTest.java

--- src/test/java/flower/store/FlowerBucketTest.java
+++ src/test/java/flower/store/FlowerBucketTest.java
@@ -11,6 +11,29 @@ public class FlowerBucketTest {
     private static final int MAX_QUANTITY = 1000;
     private static final int MAX_PRICE = 100;
 
+    private static final int RED_ROSE_SEPAL_LENGTH = 10;
+    private static final int YELLOW_TULIP_SEPAL_LENGTH = 8;
+    private static final int BLUE_TULIP_SEPAL_LENGTH = 12;
+    private static final int WHITE_ROSE_SEPAL_LENGTH = 15;
+
+    private static final double RED_ROSE_PRICE = 5.0;
+    private static final double YELLOW_TULIP_PRICE = 3.0;
+    private static final double BLUE_TULIP_PRICE = 4.0;
+    private static final double WHITE_ROSE_PRICE = 10.0;
+    private static final double RED_ROSES_PRICE = 6.0;
+
+    private static final int RED_ROSE_PACK_QUANTITY = 3;
+    private static final int YELLOW_TULIP_PACK_QUANTITY = 5;
+    private static final int BLUE_TULIP_PACK_QUANTITY = 2;
+    private static final int WHITE_ROSE_PACK_QUANTITY = 1;
+
+    private static final double PRICE_RANGE_MIN_10 = 10.0;
+    private static final double PRICE_RANGE_MAX_20 = 20.0;
+    private static final double PRICE_RANGE_MIN_20 = 20.0;
+    private static final double PRICE_RANGE_MAX_30 = 30.0;
+    private static final double PRICE_RANGE_MIN_0 = 0.0;
+    private static final double PRICE_RANGE_MAX_10 = 10.0;
+
     private FlowerBucket flowerBucket;
     private Store store;
 
@@ -31,75 +54,56 @@ public class FlowerBucketTest {
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
     }
 
-        private static final int RED_ROSE_SEPAL_LENGTH = 10;
-    private static final int YELLOW_TULIP_SEPAL_LENGTH = 8;
-    private static final int BLUE_TULIP_SEPAL_LENGTH = 12;
-    private static final int WHITE_ROSE_SEPAL_LENGTH = 15;
-
-    private static final double RED_ROSE_PRICE = 5.0;
-    private static final double YELLOW_TULIP_PRICE = 3.0;
-    private static final double BLUE_TULIP_PRICE = 4.0;
-    private static final double WHITE_ROSE_PRICE = 10.0;
-    private static final double RED_ROSES_PRICE = 6.0;
-
-    private static final double MIN_PRICE_10 = 10.0;
-    private static final double MAX_PRICE_20 = 20.0;
-    private static final double MIN_PRICE_20 = 20.0;
-    private static final double MAX_PRICE_30 = 30.0;
-    private static final double MIN_PRICE_0 = 0.0;
-    private static final double MAX_PRICE_10 = 10.0;
-
     @Test
     public void testSearchByPriceWithMultipleBuckets() {
-        FlowerBucket bucket1 = new FlowerBucket();
+        FlowerBucket bucketOne = new FlowerBucket();
         Flower redRose = new Flower(
-            FlowerColor.RED, 
-            RED_ROSE_SEPAL_LENGTH, 
-            RED_ROSE_PRICE, 
-            FlowerType.ROSE);
-        bucket1.addFlowerPack(new FlowerPack(redRose, 3));
+                FlowerColor.RED, RED_ROSE_SEPAL_LENGTH, RED_ROSE_PRICE, FlowerType.ROSE);
+        bucketOne.addFlowerPack(new FlowerPack(redRose, RED_ROSE_PACK_QUANTITY));
 
         Flower yellowTulip = new Flower(
-            FlowerColor.YELLOW, 
-            YELLOW_TULIP_SEPAL_LENGTH, 
-            YELLOW_TULIP_PRICE, 
-            FlowerType.TULIP);
-        bucket1.addFlowerPack(new FlowerPack(yellowTulip, 5));
+                FlowerColor.YELLOW, YELLOW_TULIP_SEPAL_LENGTH, 
+            YELLOW_TULIP_PRICE, FlowerType.TULIP);
+        bucketOne.addFlowerPack(new FlowerPack(yellowTulip, 
+                                               YELLOW_TULIP_PACK_QUANTITY));
 
-        store.addFlowerBucket(bucket1);
+        store.addFlowerBucket(bucketOne);
 
-        FlowerBucket bucket2 = new FlowerBucket();
+        FlowerBucket bucketTwo = new FlowerBucket();
         Flower blueTulip = new Flower(
                 FlowerColor.BLUE, BLUE_TULIP_SEPAL_LENGTH, 
             BLUE_TULIP_PRICE, FlowerType.TULIP);
-        bucket2.addFlowerPack(new FlowerPack(blueTulip, 2));
+        bucketTwo.addFlowerPack(new FlowerPack(blueTulip, 
+                                               BLUE_TULIP_PACK_QUANTITY));
 
         Flower redRoses = new Flower(
-                FlowerColor.RED, RED_ROSE_SEPAL_LENGTH, 
+                FlowerColor.RED, RED_ROSE_SEPAL_LENGTH,
             RED_ROSES_PRICE, FlowerType.ROSE);
-        bucket2.addFlowerPack(new FlowerPack(redRoses, 1));
+        bucketTwo.addFlowerPack(new FlowerPack(redRoses,
+                                               WHITE_ROSE_PACK_QUANTITY));
 
-        store.addFlowerBucket(bucket2);
+        store.addFlowerBucket(bucketTwo);
 
-        FlowerBucket bucket3 = new FlowerBucket();
+        FlowerBucket bucketThree = new FlowerBucket();
         Flower whiteRose = new Flower(
                 FlowerColor.WHITE, WHITE_ROSE_SEPAL_LENGTH, 
             WHITE_ROSE_PRICE, FlowerType.ROSE);
-        bucket3.addFlowerPack(new FlowerPack(whiteRose, 1));
+        bucketThree.addFlowerPack(new FlowerPack(whiteRose, 
+                                                 WHITE_ROSE_PACK_QUANTITY));
 
-        store.addFlowerBucket(bucket3);
+        store.addFlowerBucket(bucketThree);
 
-        List<FlowerBucket> foundBuckets = store.search(null, 
-                                                       null, 
-                                                       MIN_PRICE_10, 
-                                                       MAX_PRICE_20);
+        List<FlowerBucket> foundBuckets = store.search(null, null, 
+                                                       PRICE_RANGE_MIN_10, PRICE_RANGE_MAX_20);
         assertEquals(2, foundBuckets.size());
 
-        foundBuckets = store.search(null, null, MIN_PRICE_20, MAX_PRICE_30);
+        foundBuckets = store.search(null, null, PRICE_RANGE_MIN_20, PRICE_RANGE_MAX_30);
         assertEquals(1, foundBuckets.size());
 
-        foundBuckets = store.search(null, null, MIN_PRICE_0, MAX_PRICE_10);
+        foundBuckets = store.search(null, null, PRICE_RANGE_MIN_0, PRICE_RANGE_MAX_10);
         assertEquals(1, foundBuckets.size());
     }
+}
+
 }
 
